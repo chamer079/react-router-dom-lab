970dfd8909d29ed3055af305b7cd4309ef072b08
chamer79 <chamer79@gmail.com>
2024-11-26 19:33:00 -0500

    issue with rendering data from form

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
     <NavBar />
     <Routes>
       <Route path="/" element={<main><h1>Post Office</h1></main>} />
-      <Route path="/mailboxes" element={<MailboxList />} />
+      <Route path="/mailboxes" element={<MailboxList mailboxes={mailboxes} />} />
       <Route path="new-mailbox" element={<MailboxForm addBox={addBox}/>} />
       <Route path="mailboxes/:mailboxId" element={<MailboxDetaiils /> } />
     </Routes>
--- src/components/MailboxList.jsx
+++ src/components/MailboxList.jsx
@@ -1,11 +1,25 @@
+import { Link } from "react-router-dom"
 
 
-
-const MailboxList = () => {
-
+const MailboxList = ({mailboxes}) => {
+    console.log(mailboxes)
 
     return(
-        <h1>Mailbox List</h1>
+        <>
+         <h1>Mailbox List</h1>
+         <div>
+            {mailboxes.map((mailbox) => (
+                
+                <Link
+                 to="/mailboxes/:mailboxId"
+                 key={mailbox._id}
+                >
+                <h3>Boxholder: {mailbox.boxholder}</h3>
+                <p>Box Size: {mailbox.boxSize}</p>
+                </Link>
+            ))}
+         </div>
+        </>
     )
 }
 
