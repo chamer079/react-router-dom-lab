9ae0e29f6efb3aeb5177f0057c37c6617e5249c3
chamer79 <chamer79@gmail.com>
2024-11-26 19:03:11 -0500

    building out MailboxForm

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ import MailboxList from "./components/MailboxList"
 import MailboxForm from "./components/MailboxForm"
 import MailboxDetaiils from "./components/MailboxDetails"
 
-
 const App = () => {
   const [mailboxes, setMailboxes] = useState([])
+
+  const addBox = (newMailbox) => {
+    newMailbox._id = mailboxes.length + 1
+    setMailboxes([...mailboxes, newMailbox])
+  }
 
   return(
     <>
@@ -15,7 +19,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<main><h1>Post Office</h1></main>} />
       <Route path="/mailboxes" element={<MailboxList />} />
-      <Route path="new-mailbox" element={<MailboxForm />} />
+      <Route path="new-mailbox" element={<MailboxForm addBox={addBox}/>} />
       <Route path="mailboxes/:mailboxId" element={<MailboxDetaiils /> } />
     </Routes>
     </>
--- src/components/MailboxForm.jsx
+++ src/components/MailboxForm.jsx
@@ -1,11 +1,56 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 
 
-const MailboxForm = () => {
+const MailboxForm = (props) => {
+    const [formData, setFormData] = useState({
+        _id: "",
+        boxSize: ["Small", "Medium", "Large"],
+        boxholder: "",
+    })
 
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        props.addBox({formData})
+        setFormData({
+            _id: "",
+            boxSize: "Small",
+        })
+        navigate("/mailboxes")
+    }
+
+    const handleChange = ({ target }) => {
+        setFormData({ ...formData, [target.name]: target.value})
+    }
 
     return(
-        <h1>Mailbox Form</h1>
+        <>
+            <h1>Mailbox Form</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="boxSize-input">Box Size:</label>
+                <select
+                    name="boxSize"
+                    id="boxSize-input"
+                    onChange={handleChange}
+                >
+                    <option>Small</option>
+                    <option>Medium</option>
+                    <option>Large</option>
+                </select>
+                <label htmlFor="boxholder-input">Boxholder:</label>
+                    <input 
+                        type="text"
+                        id="boxholder-input"
+                        name="boxholder"
+                        value={formData.boxholder}
+                        onChange={handleChange}
+                    />
+                    <button type="submit">Submit</button>
+            </form>
+        </>
     )
 }
 
